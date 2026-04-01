eee6ac6f57cbe84ee6e30af111cb46f68af332b9
Chen <343137386@tdsb.ca>
2026-04-01 09:28:30 -0400

    added components files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export const App = () => {
+  return (
+    <div>
+      <h1> Cubic Equation Solver </h1>
+
+      <p> Please input 4 real numbers that represent the coefficients of a cubic function </p>
+      
+    </div>
+  );
+};
