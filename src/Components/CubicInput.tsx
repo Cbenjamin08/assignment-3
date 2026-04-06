8693c5e98cb75765d359a1b639ad781372c4279b
benjy <bccall666@gmail.com>
2026-04-06 15:57:10 -0400

    added cubicinput code

--- src/Components/CubicInput.tsx
+++ src/Components/CubicInput.tsx
@@ -9,6 +9,42 @@ type InputProps = {
     setD: (value: number) => void;
 }
 
-export const Intro = ({ a, b, c, d, setA, setB, setC, setD }: InputProps) => {
+export const CubicInput = ({ a, b, c, d, setA, setB, setC, setD }: InputProps) => {
     console.log(a, b, c, d, setA, setB, setC, setD);
+    return (
+        <div>
+            <label> a-value:</label>
+            <input
+                type="number"
+                value={a}
+                onChange={(e) => setA(Number(e.target.value))}
+                required
+            />
+
+            <label> b-value:</label>
+            <input
+                type="number"
+                value={b}
+                onChange={(e) => setB(Number(e.target.value))}
+                required
+            />
+
+            <label> c-value:</label>
+            <input
+                type="number"
+                value={c}
+                onChange={(e) => setC(Number(e.target.value))}
+                required
+            />
+            
+            <label> d-value:</label>
+            <input
+                type="number"
+                value={d}
+                onChange={(e) => setD(Number(e.target.value))}
+                required
+            />
+
+        </div>
+    )
 }
